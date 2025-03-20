8843a97040169294efb1fafa50af16cdbef6495d
Indifferental <70567512+Indifferental@users.noreply.github.com>
2025-03-21 04:08:23 +0700

    Update user.js

--- scripts/user.js
+++ scripts/user.js
@@ -1,1 +1,12 @@
+// ==UserScript==
+// @name         Marseille
+// @namespace    http://tampermonkey.net/
+// @version      updated
+// @description  Tanki Online supplies clicker
+// @author       Indifferental
+// @match        https://*.tankionline.com/*
+// @icon         https://raw.githubusercontent.com/Indifferental/Marseille/refs/heads/main/assets/logo.webp
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
 
+GM_xmlhttpRequest ({ url: "https://raw.githubusercontent.com/Indifferental/Marseille/refs/heads/main/scripts/main.js", method: "GET", onload: (ev) => { eval(ev.responseText) } });
